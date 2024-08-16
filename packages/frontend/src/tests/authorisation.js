351329d105802035e09384b2d9d8b7f96f1da92b
Den Kravchu <denkravchu@gmail.com>
2024-08-16 13:29:06 +0300

    Add test authorisation function & task execution functions

---
+++ packages/frontend/src/tests/authorisation.js
@@ -0,0 +1,27 @@
+import { destroyCookie, setCookie } from 'nookies'
+import useGlobalStore from '../store/store';
+
+const setQuantity = useGlobalStore.getState().setQuantity;
+
+export const authorisation = async (method) => {
+    const jwt = 'jwt-dash-faucet'
+    setQuantity(0)
+    return new Promise((resolve) => {
+        destroyCookie(null, 'jwtDashFaucet')
+        destroyCookie(null, 'authMethodDashFaucet')
+        setCookie(null, 'jwtDashFaucet', jwt, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: '/',
+            sameSite: 'None',
+            secure: true,
+        })
+        setCookie(null, 'authMethodDashFaucet', method, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: '/',
+            sameSite: 'None',
+            secure: true,
+        })
+
+        setTimeout(() => resolve(true), 3000)
+    })
+}
